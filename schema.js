7af1318bc3348fb9882af5f7e8d51c51c06afe93
GW <kowonp@gmail.com>
2021-11-14 04:49:08 +0900

    Divide Architecture

---
+++ schema.js
@@ -0,0 +1,11 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+
+const loadedTypeDefs = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{query,mutation}.js`);
+const typeDefs = mergeTypeDefs(loadedTypeDefs);
+const resolvers = mergeResolvers(loadedResolvers);
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
--- server.js
+++ server.js
@@ -1,60 +1,9 @@
-import { PrismaClient } from "@prisma/client";
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-
-const client = new PrismaClient();
-
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, title: String!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: async (_, { id }) => {
-      const foundMovie = await client.movie.findUnique({ where: { id } });
-      return foundMovie;
-    },
-  },
-
-  Mutation: {
-    createMovie: async (_, { title, year, genre }) => {
-      const createdMovie = await client.movie.create({ data: { title, year, genre } });
-      return createdMovie;
-    },
-    deleteMovie: async (_, { id }) => {
-      const deletedMovie = await client.movie.delete({ where: { id } });
-      return deletedMovie;
-    },
-    updateMovie: async (_, { id, title }) => {
-      const updatedMovie = await client.movie.update({ where: { id }, data: { title } });
-      console.log("updatedMovie", updatedMovie);
-      return updatedMovie;
-    },
-  },
-};
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
 });
 
