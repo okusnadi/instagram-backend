3f3dbc3ba8f71ffc356de2823bbd5f2dcbc848d4
GW <kowonp@gmail.com>
2021-11-15 02:31:55 +0900

    Create Account Part one

---
+++ users/users.typeDefs.js
@@ -0,0 +1,21 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type User {
+    id: Int!
+    firstName: String!
+    lastName: String
+    username: String!
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+
+  type Query {
+    seeProfile(username: String!): User
+  }
+
+  type Mutation {
+    createAccount(firstName: String!, lastName: String, username: String!, email: String!, password: String!): User
+  }
+`;
