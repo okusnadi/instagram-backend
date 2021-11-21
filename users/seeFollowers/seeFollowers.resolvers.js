214a15ff97c43056770d147d55f67c496665fa61
GW <kowonp@gmail.com>
2021-11-22 00:25:44 +0900

    Followers Pagination Part two

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,12 +3,23 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { username, page }) => {
-      console.log("username, page", username, page);
+      try {
+        const existingUser = await client.user.findUnique({ where: { username }, select: { username: true } });
+        if (!existingUser) {
+          return { ok: false, error: "존재하지 않는 유저입니다." };
+        }
 
-      const aFollowers = await client.user.findUnique({ where: { username } }).followers({ skip: (page - 1) * 5, take: 5 });
-      console.log("aFollowers", aFollowers);
+        const followers = await client.user.findUnique({ where: { username } }).followers({ skip: (page - 1) * 5, take: 5 });
+        if (followers.length === 0) {
+          return { ok: false, error: "팔로워가 존재하지 않습니다." };
+        }
 
-      return { ok: true, error: "팔로워 보기에 성공하였습니다.", followers: aFollowers };
+        const totalFollowers = await client.user.count({ where: { followings: { some: { username } } } });
+        return { ok: true, error: "팔로워 보기에 성공하였습니다.", followers, totalPages: Math.ceil(totalFollowers / 5) };
+      } catch (error) {
+        console.log("seeFollowers error", error);
+        return { ok: false, error: "팔로워 보기에 실패하였습니다." };
+      }
     },
   },
 };
