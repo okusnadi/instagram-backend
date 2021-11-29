dae32d1576285c46a22ed4de7684bc5abb267d84
GW <kowonp@gmail.com>
2021-11-30 03:32:41 +0900

    Create Comments typeDefs, resolvers

---
+++ comments/comments.typeDefs.js
@@ -0,0 +1,13 @@
+import { gql } from "apollo-server-core";
+
+export default gql`
+  type Comment {
+    id: Int!
+    user: User!
+    photo: Photo!
+    payload: String!
+    isMine: Boolean!
+    createdAt: String!
+    updatedAt: String!
+  }
+`;
